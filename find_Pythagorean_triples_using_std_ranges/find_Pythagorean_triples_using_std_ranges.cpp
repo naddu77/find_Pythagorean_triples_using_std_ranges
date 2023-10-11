8fe161e61baf7815f8ad40f54447a14024503334
c.pp <c.pp@navercorp.com>
2023-10-12 01:44:31 +0900

    * Replace std::views::repeat with a combination of std::views::single and std::views::take.

--- find_Pythagorean_triples_using_std_ranges/find_Pythagorean_triples_using_std_ranges.cpp
+++ find_Pythagorean_triples_using_std_ranges/find_Pythagorean_triples_using_std_ranges.cpp
@@ -5,7 +5,7 @@ int main()
     auto triples{ std::views::iota(1) | std::views::transform([](int z) {
             return std::views::iota(1, z + 1) | std::views::transform([z](int x) {
                 return std::views::iota(x, z + 1) | std::views::transform([x, z](int y) {
-                    return std::views::repeat(std::tuple{ x, y, z }, x * x + y * y == z * z ? 1 : 0);
+                    return std::views::single(std::tuple{ x, y, z }) | std::views::take(x * x + y * y == z * z ? 1 : 0);
                 }) | std::views::join;
             }) | std::views::join;
         }) | std::views::join
